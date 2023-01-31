7ddb9031ca02af6a7ae19b505225817fc8f20fdd
BelenPeCa <perezcarrizomariabelen@gmail.com>
2023-01-31 19:40:57 -0300

    Vista dinámica de ofertas y productos en el Home

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
+// ************ Require's ************
 const express = require('express');
-const app = express ();
 const path = require ('path');
+const methodOverride = require('method-override');// Para poder usar los métodos PUT y DELETE
 
-const mainRouter = require ('./routes/main.js');
-const productsRouter = require ('./routes/products.js');
-const usersRouter = require ('./routes/users.js');
-const methodOverride = require('method-override');
+// ************ express() ************
+const app = express ();
 
 
+// ************ Middlewares ************
 app.use(express.static(path.join(__dirname, '../public'))); // Necesario para los archivos estáticos
 app.use(express.urlencoded({extended: false})); // para capturar el body de los formularios
 app.use(express.json()); // capturar el body de los forms en formato JSON
+app.use(methodOverride('_method')); // Para poder usar los métodos PUT y DELETE
+
+// ************ Template Engine ************
 app.set('view engine', 'ejs'); // para indicarle que las vistas son de tipo ejs
 app.set('views', path.join(__dirname, '/views')); // Definimos ubicación de la carpeta views
-app.use(methodOverride('_method'));
+
+// ************ Route System require and use() ************
+const mainRouter = require('./routes/main.js'); // Rutas main
+const productsRouter = require('./routes/products.js'); // Rutas /products
+const usersRouter = require('./routes/users.js'); // Rutas /users
 
 app.use('/', mainRouter);
 app.use('/products', productsRouter)
@@ -22,7 +29,7 @@ app.use('/users', usersRouter)
 
 
 
-/*--------------------------------------------------------------------*/
+// ************ Set the server to listen ************
 app.listen(3002, () => {
     console.log('Server running in http://localhost:3002');
 });
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,11 +1,29 @@
 const fs = require('fs');
 const path = require('path');
-
+/* En la constante "products" ya tienen los productos que están 
+	guardados en la carpeta Data como Json (un array de objetos literales) */
+	const productsFilePath = path.join(__dirname, '../data/productsIndex.json');
+	const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 /* dentro de la variable controller listamos la lógica de cada método*/
 const controller = {
 	index: (req, res) => {
-		res.render("index");
+	const productsRead= JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+	let productsSaleFilter = productsRead.filter( producto => {
+		return producto.offer == "si, sale en home"
+	})
+
+	let productsFeaturedForHome = productsRead.filter( producto => {
+		return producto.featured == "Destacado para home"
+	})
+
+		// el primer parámetro que se pasa, es el que se utiliza en ejs
+		res.render("index", {
+			productsSale: productsSaleFilter,
+			productsFeatured: productsFeaturedForHome}
+			);
 	}
 };
 
