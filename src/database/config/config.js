11e259785864212da15466815c7bbb3b9d72866a
BelenPeCa <perezcarrizomariabelen@gmail.com>
2023-03-12 11:59:22 -0300

    Sequelize init

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": null,
     "database": "the_pets_club",
     "host": "127.0.0.1",
     "dialect": "mysql"
