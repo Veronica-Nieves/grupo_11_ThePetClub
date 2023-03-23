8c5675fce37b29ca521c6a292b1a93896ad2e089
BelenPeCa <perezcarrizomariabelen@gmail.com>
2023-03-23 18:59:00 -0300

    Avances en User

--- src/controllers/userControllerNuevo.js
+++ src/controllers/userControllerNuevo.js
@@ -1,21 +1,79 @@
 const express= require('express');
 const router = express.Router();
-
+const fs = require('fs');
+const path = require('path');
+const bcrypt = require('bcryptjs')
 /* Variable creada por sequelize para conectar la base de datos */
 const db  = require("../database/models")
+/* Asocia el modelo "User" de la base de datos "db" */
+const User = require("../database/models/User");
 
 const controller = {
-    list: function(){
+    /* Busca todos los usuarios y los muestra en la ruta /list */
+    list: function(req, res){
         db.User.findAll()
         .then(function(users){
-            res.render("user-list", {usuarios:users})
+            res.render("user-list", {usuarios: users})
+            console.log()
         })
     },
-    detailUser: function(){
+    /* Busca todos los usuarios y los muestra en la ruta /list/:id */
+    detailUser: function(req, res){
         db.User.findbyPK(req.params.id)
         .then(function(users){
             res.render("user-detail", {usuarios:users})
         })
+    },
+    /* Renderiza el formulario para crear un registro de usuario */
+    add: function(req, res){
+        return res.render('./users/register');
+    },  
+    /* Crea el nuevo usuario si es que pasa las validaciones */
+    create: function(req, res){
+        
+        db.User.create({
+            fisrt_name: req.body.firstName,
+            last_name: req.body.lastName,
+            user_name: req.body.nameUser,
+            email: req.body.email,
+            password: req.body.password,
+            password_confirmed: req.body.passwordConfirmed,
+            avatar: req.file ? req.file.filename : '',
+            rol_id: req.body.role
+        });
+        res.redirect('/users/login');
+    },
+    edit: function(req, res) {
+        db.User.findbyPK(req.params.id)
+        .then(function(users){
+            res.render("user-edit", {usuarios:users})
+        })
+    },
+    update: function(req, res){
+        db.User.update({
+            fisrt_name: req.body.firstName,
+            last_name: req.body.lastName,
+            /* Para actualizar la contraseña, se debe validar la anterior contraseña (PENDIENTE)
+            password: bcrypt.hashSync(req.body.password, 10),
+            password_confirmed: bcrypt.hashSync(req.body.password, 10), */
+            avatar: req.file ? req.file.filename : '',
+            rol_id: req.body.role
+        }, {
+            where: {
+                /* Se utiliza params porque es la manera de acceder a parametros que nos llegan en la url */
+                id: req.params.id
+            }
+        });
+        res.redirect('/users/profile/' + req.params.id);
+    },
+    delete: function(req, res){
+        db.User.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.redirect('/users/list');
     }
 };
 
+module.exports = controller;
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,11 +2,10 @@ const express= require('express');
 const router = express.Router();
 const path = require('path');
 
-/* Requiero la base de datos */
-const db = require('../database/models/');
 /* Controller */
 /* const usersController = require('../controllers/usersController'); */
-/* nuevo controlador */
+
+/* Nuevo controlador */
 const usersController = require('../controllers/userControllerNuevo');
 const { body } = require('express-validator');
 
@@ -24,22 +23,30 @@ router.get('/list', usersController.list);
 /* Ruta de detalle de usuario */
 router.get('/list/:id', usersController.detailUser);
 
-// Formulario de registro
-router.get('/register/', usersController.register);
+/* Ruta que se encarga de mostrar el formulario para crear un nuevo registro de usuario */
+router.get('/register/', usersController.add);
 
-// Procesar el registro
-router.post('/register/', uploadFile.single('avatar'), validations, usersController.processRegister);
+/* Ruta que se encarga de mostrar el formulario y crear un nuevo registro de usuario */
+router.post('/register/',/*  uploadFile.single('avatar'), validations, */ usersController.create/* , usersController.processRegister */);
 
+/* Ruta que se encarga de mostrar el formulario para modificar un usuario ya registrado*/
+router.get('/edit/:id', usersController.edit);
+
+/* Ruta que se encarga de modificar los datos de un usuario ya registrado*/
+router.post('/edit/:id', usersController.update);
+
+/* Ruta que se encarga de modificar los datos de un usuario ya registrado*/
+router.post('/delete/:id', usersController.delete);
 
 /* ---------------------RUTAS DE USERS-LOGIN ------------------------*/
 
 // Formulario de login
-router.get('/login/', usersController.login);
-router.post('/login/', uploadFile.single("image") , usersController.processLogin);
+router.get('/login/'/* , usersController.login */);
+//router.post('/login/'/* , uploadFile.single("image") , usersController.processLogin */);
 
 // Perfil del usuario
-router.get('/profile/', guestMiddleware, usersController.profile);
-router.get('/profile/:userId', usersController.profile);
+//router.get('/profile/'/* , guestMiddleware, usersController.profile */);
+//router.get('/profile/:userId'/* , usersController.profile */);
 
 /* ---------------- Fin Listado de rutas -------------------*/
 module.exports = router;
