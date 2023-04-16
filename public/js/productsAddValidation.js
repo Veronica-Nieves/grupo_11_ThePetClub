98a16b38ab6419272ab533c314e37e52416fb884
Veronica-Nieves <v.nievescruz@gmail.com>
2023-04-15 23:26:39 -0600

    AValidación Fron-End Productos

--- public/js/productsAddValidation.js
+++ public/js/productsAddValidation.js
@@ -2,13 +2,19 @@ window.onload = function(){
 
     console.log("Estamos en la consola de la validación Front")
     let form = document.querySelector(".form-container");
-    let errorsHtml = document.querySelector(".errores");
+    let warningBlock = document.querySelector(".warning-block")
+    let warningTitle = document.querySelector(".warning-title")
+    let errorsHtml = document.querySelector(".errors");
     let cleanFormButton = document.querySelector(".clean-form")
 
+    // Identifica si una cadena contiene espacios en blanco
     let regWhiteSpace = /\s/; 
-    let regSpecialCaracters = /^([a-z0-9]+(\/{1}[a-z0-9]+)*)+(?!([\/]{2}))$/; 
-    let regAlphaNum = /^[A-Za-z\s]+$/;
-    
+    // Identifica cadenas alfanuméricas (Solo caracteres dentro del rango)
+    let regAlphaNum = /[A-Za-z0-9]/;
+    // Identifica cadenas con caracteres especiales (detecta cualquier caracter que No entre en el rango [A-Za-z0-9])
+    let regSpecialCaracter = /[^A-Za-z0-9]/; 
+
+
 //-------------------VALIDACION FRONT PARA UN NUEVO PRODUCTO --------------------//
 
 
@@ -17,15 +23,15 @@ window.onload = function(){
     form.addEventListener("submit", (event) => {
         event.preventDefault();
 
-        let errors = ["¡¡ ADVERTENCIA !!\n"];
+        let errors = [];
 
     // NOMBRE
         if (form.name.value == "") {
-            errors.push("Debe ingresar un nombre");
+            errors.push("Debe ingresar un nombre.");
             form.name.classList.remove("is-valid")
             form.name.classList.add("is-invalid")
         } else if (form.name.value.length < 3 ) {
-            errors.push("El nombre debe tener al menos 3 caracteres");
+            errors.push("El nombre debe tener al menos 3 caracteres.");
             form.name.classList.remove("is-valid")
             form.name.classList.add("is-invalid")
         } else {
@@ -37,35 +43,25 @@ window.onload = function(){
 
     // SKU
         if (form.sku.value == "") {
-            errors.push("Debe ingresar un SKU con al menos 3 caracteres");
+            errors.push("Debe ingresar un SKU.");
             form.sku.classList.remove("is-valid")
             form.sku.classList.add("is-invalid")
-        } else if (form.sku.value.length < 3 ) {
-            errors.push("El SKU debe tener al menos 3 caracteres");
+        } else if ( form.sku.value.length < 3 || regWhiteSpace.test(form.sku.value) || regSpecialCaracter.test(form.sku.value)) {
+            errors.push("El SKU debe tener al menos 3 caracteres, no debe incluir espacios en blanco ni caracteres especiales.");
             form.sku.classList.remove("is-valid")
             form.sku.classList.add("is-invalid")
-        } else if (regWhiteSpace.test(form.sku.value) ) {
-            errors.push("El SKU no debe incluir espacios en blanco");
-            form.sku.classList.remove("is-valid")
-            form.sku.classList.add("is-invalid")
-            /* Falta corregir que No acepte caracteres especiales
-        } else if (NOT(regAlphaNum.test(form.sku.value)) ) {
-            errors.push("El SKU no debe incluir caracteres especiales");
-            form.sku.classList.remove("is-valid")
-            form.sku.classList.add("is-invalid")
-            */
         } else {
-            form.sku.classList.remove("is-invalid")
-            form.sku.classList.add("is-valid")
+            form.name.classList.remove("is-invalid")
+            form.name.classList.add("is-valid")
         }
 
     // DESCRIPCIÓN
     if (form.description.value == "") {
-        errors.push("Debe ingresar la descripción del producto");
+        errors.push("Debe ingresar la descripción del producto.");
         form.description.classList.remove("is-valid")
         form.description.classList.add("is-invalid")
     } else if (form.description.value.length < 10 ) {
-        errors.push("El nombre debe tener al menos 10 caracteres");
+        errors.push("El nombre debe tener al menos 10 caracteres.");
         form.description.classList.remove("is-valid")
         form.description.classList.add("is-invalid")
     } else {
@@ -75,11 +71,11 @@ window.onload = function(){
 
     // PRECIO
     if (form.price.value == "") {
-        errors.push("Ingresar el precio del producto");
+        errors.push("Indique el precio del producto.");
         form.price.classList.remove("is-valid")
         form.price.classList.add("is-invalid")
     } else if ( !(Number(form.price.value)) || form.price.value <= 0) {
-        errors.push("El precio debe ser un valor numérico mayor que cero");
+        errors.push("El precio debe ser un valor numérico mayor que cero.");
         form.price.classList.remove("is-valid")
         form.price.classList.add("is-invalid")
     } else {
@@ -87,17 +83,15 @@ window.onload = function(){
         form.price.classList.add("is-valid")
     }
 
+    console.log("opcion oferta")
+    console.log(form.offer.value)
     // PRECIO DE OFERTA
-    if (form.priceOffer.value == "") {
-        errors.push("Ingresar el precio de oferta del producto. Solo se mostratrá si indica que el producto está en oferta");
+    if (form.priceOffer.value == "" && form.offer.value == "si") {
+        errors.push("Indique el precio de oferta del producto (solo se mostratrá si indica que el producto está en oferta)");
         form.priceOffer.classList.remove("is-valid")
         form.priceOffer.classList.add("is-invalid")
-    } else if ( !(Number(form.priceOffer.value)) || form.priceOffer.value <= 0) {
-        errors.push("El precio de oferta debe ser un valor numérico mayor que cero");
-        form.priceOffer.classList.remove("is-valid")
-        form.priceOffer.classList.add("is-invalid")
-    } else if ( form.priceOffer.value >= form.price.value) {
-        errors.push("El precio de oferta debe ser menor o igual que el precio regular del producto");
+    } else if ( !(Number(form.priceOffer.value)) || form.priceOffer.value <= 0 || form.priceOffer.value >= form.price.value ) {
+        errors.push("El precio de oferta debe ser un valor numérico mayor que cero y menor que el precio regular del producto.");
         form.priceOffer.classList.remove("is-valid")
         form.priceOffer.classList.add("is-invalid")
     } else {
@@ -105,13 +99,14 @@ window.onload = function(){
         form.priceOffer.classList.add("is-valid")
     }
 
+
      // PIECES
     if (form.pieces.value == "") {
-        errors.push("Debe ingregar el inventario del producto");
+        errors.push("Indique el inventario del producto.");
         form.pieces.classList.remove("is-valid")
         form.pieces.classList.add("is-invalid")
     } else if ( !(Number(form.pieces.value)) || form.pieces.value <= 0) {
-        errors.push("El inventario debe ser un valor numérico mayor que cero");
+        errors.push("El inventario debe ser un valor numérico mayor que cero.");
         form.pieces.classList.remove("is-valid")
         form.pieces.classList.add("is-invalid")
     } else {
@@ -122,14 +117,17 @@ window.onload = function(){
     
     // Mostrar Errores
     console.log(errors);
-    if (errors.length > 0) {
+    if (errors.length > 1) {
 
         event.preventDefault();
         errorsHtml.innerHTML = "";
         errors.forEach(error => {
-            errorsHtml.innerHTML += "<li>" + error + "</li>"
+            errorsHtml.innerHTML += "<li> <i class='fa-solid fa-triangle-exclamation' style='color: #eb0505;'></i>  " + error + "</li>"
         });
-        errorsHtml.classList.remove("hide")
+        warningBlock.classList.remove("hide");
+        warningTitle.classList.remove("hide");
+        errorsHtml.classList.remove("hide");
+
     } else {
         errorsHtml.innerHTML = "";
         form.submit();
@@ -140,8 +138,18 @@ window.onload = function(){
 
     /* ------ LIMPIAR FORMULARIO -------*/
     cleanFormButton.addEventListener('click', function(){
+
         errorsHtml.innerHTML = "";
-        errorsHtml.classList.add("hide")
+        warningBlock.classList.add("hide");
+        warningTitle.classList.add("hide");
+        errorsHtml.classList.add("hide");
+    
+        form.name.classList.remove("is-invalid");
+        form.sku.classList.remove("is-invalid");
+        form.description.classList.remove("is-invalid");
+        form.price.classList.remove("is-invalid");
+        form.priceOffer.classList.remove("is-invalid");
+        form.pieces.classList.remove("is-invalid");
 
     });
 
