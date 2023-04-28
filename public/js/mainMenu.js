5aa2fba53f64ece26ea54fe3de3519343b09ff26
Veronica-Nieves <v.nievescruz@gmail.com>
2023-04-27 22:48:49 -0600

    Conexion de menú desplegable

---
+++ public/js/mainMenu.js
@@ -0,0 +1,10 @@
+/* En este archivo cpmienzo con las pruebas de mostrar y esconder el menu que diseñó Lu */
+window.addEventListener("load", () => {
+    alert("Alerta al cargar")
+    console.log("Cargamos el sitio")
+
+    
+
+
+
+})
